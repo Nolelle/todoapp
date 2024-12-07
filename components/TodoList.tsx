4b3ae9878ebab872394fd53b57e544b3ada97469
Nolelle <eyu1@ualberta.ca>
2024-12-07 16:15:07 -0700

    refactor: enhance TodoList component with TypeScript types and improved delete functionality

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,19 +1,30 @@
-import TodoItem from "./TodoItem";
+"use client";
 
-export default function TodoList({ todos, setTodos }) {
+interface Todo {
+  id: string;
+  title: string;
+}
+
+interface TodoListProps {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+export default function TodoList({ todos, setTodos }: TodoListProps) {
   const deleteTodo = async (id: string) => {
-    await fetch(`/api/todos/${id}`, { method: "DELETE" });
-    setTodos(todos.filter((todo) => todo.id !== id));
+    await fetch(`/api/todos/${id}`, {
+      method: "DELETE"
+    });
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
   };
 
   return (
     <ul>
       {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          onDelete={deleteTodo}
-        />
+        <li key={todo.id}>
+          {todo.title}
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        </li>
       ))}
     </ul>
   );
